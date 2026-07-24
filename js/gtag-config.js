09fcdf0817f180155af418512c51b6c352d61071
Myraa Developer <myraa@myraasafetynets.com>
2026-07-24 13:03:06 +0530

    updated gtag

---
+++ js/gtag-config.js
@@ -0,0 +1,10 @@
+// Google tag (gtag.js) - Centralized Configuration
+var script = document.createElement('script');
+script.async = true;
+script.src = 'https://www.googletagmanager.com/gtag/js?id=AW-18280852924';
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'AW-18280852924');
